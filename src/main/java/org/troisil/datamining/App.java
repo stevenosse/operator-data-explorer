46894c5915af12b6baf92acc0c3665d9fd17fb2e
Steve Nosse <nossesteve@gmail.com>
2020-12-03 14:35:01 +0100

    added spark session initialisation

--- src/main/java/org/troisil/datamining/App.java
+++ src/main/java/org/troisil/datamining/App.java
@@ -3,6 +3,8 @@ package org.troisil.datamining;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
 
 /**
  * Hello world!
@@ -14,6 +16,18 @@ public class App
     public static void main( String[] args )
     {
         Config config = ConfigFactory.load("application.conf");
+
+        SparkConf sparkConf = new SparkConf()
+                .setMaster(config.getString("app.master"))
+                .set("executor.cores", config.getString("app.executor.cores"))
+                .set("executor.memory", config.getString("app.executor.memory"))
+                .set("executor.instances", config.getString("app.executor.instances"));
+
+        SparkSession sparkSession = SparkSession.builder()
+                .config(sparkConf)
+                .getOrCreate();
+
+
         log.info("See! It's logging.");
     }
 }
