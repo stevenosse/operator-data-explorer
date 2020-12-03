95b7fa4d1d2d3a966b6f2ce61eaab677bb0b0bc6
azolou <azoloujoviale@gmail.com>
2020-12-03 17:46:55 +0100

    Testing CSV Writer

--- src/main/java/org/troisil/datamining/DatasetCsvWriter.java
+++ src/main/java/org/troisil/datamining/DatasetCsvWriter.java
@@ -2,18 +2,19 @@ package org.troisil.datamining;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 
 import java.util.function.Consumer;
 
 @Slf4j
 @RequiredArgsConstructor
-public class DatasetCsvWriter<T> implements Consumer<Dataset<T>> {
+public class DatasetCsvWriter<Row> implements Consumer<Dataset<Row>> {
     private final String outputPathStr;
 
     @Override
-    public void accept(Dataset<T> tDataset) {
-        Dataset<T> ds = tDataset.cache();
+    public void accept(Dataset<Row> tDataset) {
+        Dataset<Row> ds = tDataset.cache();
         log.info("Writing data ds.count()={} into outputPathStr = {}", ds.count(), outputPathStr);
         ds.printSchema();
         ds.show(5);
