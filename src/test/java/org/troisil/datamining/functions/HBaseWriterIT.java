99aaf3b5ef6fa19251ce90e699d1ec2c849ad9d8
Steve Nosse <nossesteve@gmail.com>
2021-01-06 13:34:25 +0100

    integrating hbase

--- src/test/java/org/troisil/datamining/functions/HBaseWriterIT.java
+++ src/test/java/org/troisil/datamining/functions/HBaseWriterIT.java
@@ -1,40 +1,89 @@
 package org.troisil.datamining.functions;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.var;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.junit.BeforeClass;
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class HBaseWriterIT {
-    private static final Config testConfig = ConfigFactory.load("application.conf");
-    private static SparkSession sparkSession;
 
-    @BeforeClass
-    public static void setUp() throws IOException {
-        sparkSession = SparkSession.builder()
-                .master(testConfig.getString("app.master"))
-                .appName(testConfig.getString("app.name"))
-                .getOrCreate();
-    }
+    private final String catalogName = "target/test-classes/catalog.json";
+
+    private final String catalog = "{\n" +
+            "  \"table\": {\n" +
+            "    \"namespace\": \"tourism\",\n" +
+            "    \"name\": \"coordonnees\"\n" +
+            "  },\n" +
+            "  \"rowkey\": \"key\",\n" +
+            "  \"columns\": {\n" +
+            "    \"key\": {\n" +
+            "      \"cf\": \"rowkey\",\n" +
+            "      \"col\": \"key\",\n" +
+            "      \"type\": \"string\"\n" +
+            "    },\n" +
+            "    \"Latitude\": {\n" +
+            "      \"cf\": \"geo\",\n" +
+            "      \"col\": \"latitude\",\n" +
+            "      \"type\": \"double\"\n" +
+            "    },\n" +
+            "    \"Longitude\": {\n" +
+            "      \"cf\": \"geo\",\n" +
+            "      \"col\": \"longitude\",\n" +
+            "      \"type\": \"double\"\n" +
+            "    },\n" +
+            "    \"Adresse_postale\": {\n" +
+            "      \"cf\": \"loc\",\n" +
+            "      \"col\": \"address\",\n" +
+            "      \"type\": \"string\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
     @Test
-    public void testWriter() {
-        var datasetCsvReader = new DatasetCsvReader(sparkSession, testConfig.getString("app.data.input"));
-        SiteCountPerOperator siteCountPerOperator = new SiteCountPerOperator();
-        var ds = siteCountPerOperator.apply(datasetCsvReader.get());
-        ds.printSchema();
-        var writer = new HBaseWriter("target/test-classes/catalog.json");
-        writer.accept(ds);
+    public void testWriter(){
+        log.info("running hbaseWriter test");
+        SparkSession sparkSession = SparkSession.builder().master("local[2]").appName("test-reader").getOrCreate();
+
+        List<HBaseRow> expected = Arrays.asList(
+                HBaseRow.builder().key("k1").operateur("orange").region("haute-vienne").nb_sites_2g(1).nb_sites_3g(2).nb_sites_4g(1).build(),
+                HBaseRow.builder().key("k2").operateur("bouygues").region("haute-vienne").nb_sites_2g(1).nb_sites_3g(2).nb_sites_4g(1).build()
+        );
+
+        Dataset<Row> expectedData = sparkSession.createDataset(expected, Encoders.bean(HBaseRow.class)).toDF();
+        expectedData.printSchema();
+        expectedData.show();
+
+        new HBaseWriter(catalogName).accept(expectedData);
+
+        Dataset<HBaseRow> actualData = sparkSession.read()
+                .option(HBaseTableCatalog.tableCatalog(), catalog)
+                .format("org.apache.spark.sql.execution.datasources.hbase")
+                .load().as(Encoders.bean(HBaseRow.class));
+
+        assertThat(actualData.collectAsList()).containsExactlyElementsOf(expected);
+    }
+
+    @Data @Builder @AllArgsConstructor @NoArgsConstructor
+    public static class HBaseRow implements Serializable {
+        private String key;
+        private String operateur;
+        private String region;
+        private Integer nb_sites_2g;
+        private Integer nb_sites_3g;
+        private Integer nb_sites_4g;
     }
 }
